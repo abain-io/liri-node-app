baac8a77abc4c239d3239e0b04f3990dbdb58e5b
dolphinman <wpfan.org@gmail.com>
2018-05-15 09:15:04 -0500

    updated JS file

--- liri.js
+++ liri.js
@@ -1,9 +1,36 @@
 require("dotenv").config();
+var spotify = require('spotify');
+var Twitter = require('twitter');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var nodeArray = process.argv;
 console.log(nodeArray[3]);
 
-var movieName = "Batman";
+var movieName = "";
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+
+
+
+
+
+
+
+
+ 
+var spotify = new Spotify({
+  id: <e94830ea9a5c408eab0281327138eb37>,
+  secret: <09d77010ae7c42aeb6df29e7f3072776>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+
