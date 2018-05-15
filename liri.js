ca9d794df6e464f6d4e30862f3128cf008da2598
dolphinman <wpfan.org@gmail.com>
2018-05-15 09:30:45 -0500

    updated JS info and added mroe code

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
 
 var nodeArray = process.argv;
 console.log(nodeArray[3]);
@@ -21,8 +23,8 @@ var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey
 
  
 var spotify = new Spotify({
-  id: <e94830ea9a5c408eab0281327138eb37>,
-  secret: <09d77010ae7c42aeb6df29e7f3072776>
+  id: "e94830ea9a5c408eab0281327138eb37",
+  secret: "09d77010ae7c42aeb6df29e7f3072776"
 });
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
@@ -31,6 +33,9 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
   }
  
 console.log(data); 
+
+console.log(data.tracks.items[0]);
+
 });
 
 
